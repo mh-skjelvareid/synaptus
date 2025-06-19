2311cd6041414963022edbb594fc824433bcf99c
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-06-19 16:37:24 +0200

    Adding first functions to API

---
+++ python/synaptus/__init__.py
@@ -0,0 +1,7 @@
+from .utils import get_envelope, log_image, nextpow2, plot_us_image
+
+# Exported API symbols
+__all__ = ["log_image", "get_envelope", "plot_us_image", "nextpow2"]
+
+# Package version
+__version__ = "0.2.0"
