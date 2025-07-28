71280d8993bbf6e1bb3d154e33d21d0d9b7f368b
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-07-28 23:25:37 +0200

    Add additional functions used in main file

--- python/synaptus/__init__.py
+++ python/synaptus/__init__.py
@@ -1,7 +1,32 @@
-from .utils import get_envelope, log_image, nextpow2, plot_us_image
+from .datasets import load_mat_dataset
+from .utils import (
+    calc_depth_resolution,
+    calc_omega_passband,
+    get_envelope,
+    log_image,
+    make_coord_grids,
+    make_k_vec,
+    make_kz_grid,
+    make_omega_vec,
+    nextpow2,
+    plot_us_image,
+)
 
 # Exported API symbols
-__all__ = ["log_image", "get_envelope", "plot_us_image", "nextpow2"]
+__all__ = [
+    "calc_depth_resolution",
+    "calc_omega_passband",
+    "get_envelope",
+    "load_mat_dataset",
+    "log_image",
+    "make_coord_grids",
+    "make_k_vec",
+    "make_kz_grid",
+    "make_omega_vec",
+    "nextpow2",
+    "plot_us_image",
+]
+
 
 # Package version
 __version__ = "0.2.0"
