83a0ee04e1478ccc32457fc80590410292ea39c8
Martin H. Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-10-16 12:46:01 +0200

    Adding tests for 3D multilayer dataset

--- python/tests/test_psm_mulok_end_to_end.py
+++ python/tests/test_psm_mulok_end_to_end.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 from synaptus import (
     MultilayerCartesianPulseEchoData,
     MultilayerOmegaKMigration,
@@ -38,7 +40,7 @@ def test_phase_shift_migration_2d():
     )
     images, z_vecs = psm.phase_shift_migrate()
     for image, z_vec in zip(images, z_vecs):
-        plot_us_image(image, y_val=z_vec)
+        plot_us_image(image, y_val=z_vec, title="PSM focused image, 2D, single layer")
 
 
 def test_mulok_2d():
@@ -55,7 +57,76 @@ def test_mulok_2d():
         t_delay=mat_dataset.t_delay,
         wave_velocities=mat_dataset.wave_vel,
     )
-
     images, z_vecs = mulok.mulok_migrate()
     for image, z_vec in zip(images, z_vecs):
-        plot_us_image(image, y_val=z_vec)
+        plot_us_image(image, y_val=z_vec, title="MULOK focused image, 2D, single layer")
+
+
+def test_psm_3d():
+    """Test the PhaseShiftMigration class with 3D data."""
+    mat_dataset = load_mat_dataset("PlaneScan3D_PlexiAluFBH.mat")
+
+    psm = PhaseShiftMigration(
+        raw_data=mat_dataset.raw_data,
+        fs=mat_dataset.fs,
+        f_low=mat_dataset.f_low,
+        f_high=mat_dataset.f_high,
+        x_step=mat_dataset.x_step,  # type:ignore
+        y_step=mat_dataset.y_step,  # type:ignore
+        t_delay=mat_dataset.t_delay,
+        wave_velocities=mat_dataset.wave_vel,
+        layer_thicknesses=mat_dataset.layer_thick,
+    )
+
+    # Perform migration (focusing)
+    images, z_vecs = psm.phase_shift_migrate()
+
+    # Create C-scan images plexiglas and aluminium by depth gating
+    z_ind_plexi = (z_vecs[1] >= 0.068) & (z_vecs[1] <= 0.092)  # Depth gating, plexiglas layer
+    z_ind_alu = (z_vecs[2] >= 0.1) & (z_vecs[2] <= 0.14)  # Depth gating, aluminium layer
+
+    cscan_im_plexi = images[1][z_ind_plexi, :, :].max(axis=0)
+    cscan_im_alu = images[2][z_ind_alu, :, :].max(axis=0)
+
+    # Plot C-scan images
+    fig, ax = plt.subplots(1, 2, figsize=(5, 5))
+    ax[0].imshow(cscan_im_plexi)
+    ax[1].imshow(cscan_im_alu)
+    ax[0].set_title("C-scan, PSM, plexiglas")
+    ax[1].set_title("C-scan, PSM, aluminium")
+    plt.show()
+
+
+def test_mulok_3d():
+    """Test the MultilayerOmegaKMigration class with 3D data."""
+    mat_dataset = load_mat_dataset("PlaneScan3D_PlexiAluFBH.mat")
+
+    mulok = MultilayerOmegaKMigration(
+        raw_data=mat_dataset.raw_data,
+        fs=mat_dataset.fs,
+        f_low=mat_dataset.f_low,
+        f_high=mat_dataset.f_high,
+        x_step=mat_dataset.x_step,  # type:ignore
+        y_step=mat_dataset.y_step,  # type:ignore
+        t_delay=mat_dataset.t_delay,
+        wave_velocities=mat_dataset.wave_vel,
+        layer_thicknesses=mat_dataset.layer_thick,
+    )
+
+    # Perform migration (focusing)
+    images, z_vecs = mulok.mulok_migrate()
+
+    # Create C-scan images plexiglas and aluminium by depth gating
+    z_ind_plexi = (z_vecs[1] >= 0.068) & (z_vecs[1] <= 0.092)  # Depth gating, plexiglas layer
+    z_ind_alu = (z_vecs[2] >= 0.1) & (z_vecs[2] <= 0.14)  # Depth gating, aluminium layer
+
+    cscan_im_plexi = images[1][z_ind_plexi, :, :].max(axis=0)
+    cscan_im_alu = images[2][z_ind_alu, :, :].max(axis=0)
+
+    # Plot C-scan images
+    fig, ax = plt.subplots(1, 2, figsize=(5, 5))
+    ax[0].imshow(cscan_im_plexi)
+    ax[1].imshow(cscan_im_alu)
+    ax[0].set_title("C-scan, MULOK, plexiglas")
+    ax[1].set_title("C-scan, MULOK, aluminium")
+    plt.show()
